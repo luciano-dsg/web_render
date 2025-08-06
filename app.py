219ce9aec02d65b8b6ec47044d9595aeb522061d
luciano-dsg <138631651+luciano-dsg@users.noreply.github.com>
2025-08-06 09:02:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ st.header('Jogando uma moeda')
 
 st.write('Ainda não é um aplicativo funcional. Em construção.')
 
-$ ls -a
-.git .gitignore .streamlit app.py README.md requirements.txt
-$ git commit -am "initial commit"
-$ git push
+
 
